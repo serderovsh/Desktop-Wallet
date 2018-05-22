bee1e0c8cdedc145f8701267724a683a8516b413
Antoine Portaria <antoine@hakouna.com>
2018-05-22 16:24:39 -0400

    Create Main.js

---
+++ app/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { routes } from "../routes";
+import styles from '../components/ContentMain.css';
+
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className={styles.contentSecondary}>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Main;
