142df8a4cf451eff4e7aaefc9bf752d7519e47e3
Magalon <antoine@hakouna.com>
2018-05-23 16:17:07 -0400

    Representatives

--- app/reducers/representatives.js
+++ app/reducers/representatives.js
@@ -1,38 +1,7 @@
 import { SET_REPRESENTATIVES } from '../actions/representatives';
 
 const initialState = {
-  representatives: [
-    // {
-    //   voteTitle: 'https://twitter.com/tronfund/3087340756334730567',
-    //   lastBlock: '65986',
-    //   blocksProduced: '5274',
-    //   blocksMissed: '2400000'
-    // },
-    // {
-    //   voteTitle: 'https://twitter.com/tronfund/3087340756334730567',
-    //   lastBlock: '65986',
-    //   blocksProduced: '5274',
-    //   blocksMissed: '2400000'
-    // },
-    // {
-    //   voteTitle: 'https://twitter.com/tronfund/3087340756334730567',
-    //   lastBlock: '65986',
-    //   blocksProduced: '5274',
-    //   blocksMissed: '2400000'
-    // },
-    // {
-    //   voteTitle: 'https://twitter.com/tronfund/3087340756334730567',
-    //   lastBlock: '65986',
-    //   blocksProduced: '5274',
-    //   blocksMissed: '2400000'
-    // },
-    // {
-    //   voteTitle: 'https://twitter.com/tronfund/3087340756334730567',
-    //   lastBlock: '65986',
-    //   blocksProduced: '5274',
-    //   blocksMissed: '2400000'
-    // }
-  ],
+  representatives: [],
 };
 
 export function representativeReducer(state = initialState, action) {
@@ -46,8 +15,4 @@ export function representativeReducer(state = initialState, action) {
     default:
       return state;
   }
-}
-
-export function getWitnesses(state: State) {
-  return state.allIds.map(id => state.byId[id]);
 }
