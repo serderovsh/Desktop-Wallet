ee3c821772aeddb44d09108b3b045c047a2ec21a
Magalon <antoine@hakouna.com>
2018-05-25 11:37:44 -0400

    Contacts reducers and actions startup

---
+++ app/reducers/contacts.js
@@ -0,0 +1,30 @@
+import { ADD_CONTACT, REMOVE_CONTACT, UPDATE_CONTACT} from '../actions/contacts';
+
+const initialState = {
+  contactName: '',
+  contactAddress: '',
+  contactImage: '',
+};
+
+
+export function contactReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_CONTACT: {
+      return {
+        ...state,
+      };
+    }
+    case REMOVE_CONTACT: {
+      return {
+        ...state,
+      };
+    }
+    case UPDATE_CONTACT: {
+      return {
+        ...state,
+      };
+    }
+    default:
+      return state;
+  }
+}
