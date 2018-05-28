eff839feef11bf72e6f801c5dc8417fe1c015f73
Tommy P <thomaspud@gmail.com>
2018-05-27 20:45:48 -0400

    Added: Scaffolding for Settings Sub-Categories, Finished Share and Support

--- app/components/Settings/Notifications/index.js
+++ app/components/Settings/Notifications/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from '../../Wallet/Send/Send.css';
+import styles from './Notifications.css';
 
 import Secondary from '../../Content/Secondary';
 import Header from '../../Header';
-import Background from '../../ContentSecondaryBG';
-import { MoreIcon, CalendarIcon, VoteIcon } from '../../Icons';
+
+import { Form, TextArea, Button } from 'semantic-ui-react'
+import buttonStyles from '../../Button.css';
+
+import { CommentIcon } from '../../Icons';
 
 export default class Notifications extends Component {
   render() {
     return (
-      <Secondary className={styles.container}>
+      <Secondary>
         <Header headerName="Notifications" />
+        <Form className={styles.container}>
+          <CommentIcon className={styles.icon} />
+          <div>Write below and click on submit to send a support message.</div>
+          <Form.TextArea placeholder="Write your message here..." className={styles.textArea} />
+          <Form.Button className={`${styles.btn} ${buttonStyles.button} ${buttonStyles.black}`}>Submit</Form.Button>
+        </Form>
       </Secondary>
     );
   }
--- app/components/Settings/Share/index.js
+++ app/components/Settings/Share/index.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from '../../Wallet/Send/Send.css';
+import styles from './Share.css';
 
 import Secondary from '../../Content/Secondary';
 import Header from '../../Header';
-import Background from '../../ContentSecondaryBG';
-import { MoreIcon, CalendarIcon, VoteIcon } from '../../Icons';
+
+import { Button } from 'semantic-ui-react'
+import buttonStyles from '../../Button.css';
+
+import { ShareIcon, RedditIcon, TwitterIcon, FacebookIcon } from '../../Icons';
 
 export default class Share extends Component {
   render() {
     return (
-      <Secondary className={styles.container}>
-        <Header headerName="Share TronWatch" />
+      <Secondary>
+        <Header headerName="Share" />
+        <div className={styles.container}>
+          <ShareIcon className={styles.icon} />
+          <div>Share Tron Wallet on social media.</div>
+          <div className={styles.subText}>Click one of the buttons below to be taken to it's respective website to share a post.</div>
+          <div className={styles.buttonContainer}>
+            <Button className={`${buttonStyles.iconButton} ${buttonStyles.gradient}`}><FacebookIcon /></Button>
+            <Button className={`${buttonStyles.iconButton} ${buttonStyles.gradient}`}><TwitterIcon /></Button>
+            <Button className={`${buttonStyles.iconButton} ${buttonStyles.gradient}`}><RedditIcon /></Button>
+          </div>
+        </div>
       </Secondary>
     );
   }
