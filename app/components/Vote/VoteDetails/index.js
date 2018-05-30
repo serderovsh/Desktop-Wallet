2a4ef9f48afe74b9100434cbbef5421ff7d4c812
Till Mueller <metjm.mail@gmail.com>
2018-05-31 01:56:10 +0200

    vote modal

--- app/components/Vote/VoteDetails/index.js
+++ app/components/Vote/VoteDetails/index.js
@@ -1,116 +1,219 @@
-import React, { Component } from 'react';
-import { Dropdown, Button } from 'semantic-ui-react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {Dropdown, Button} from 'semantic-ui-react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 import styles from './VoteDetails.css';
 import buttonStyles from '../../Button.css';
-import { loadWitnesses } from '../../../actions/witnesses';
+import {loadWitnesses} from '../../../actions/witnesses';
 import Secondary from '../../Content/Secondary';
 import Header from '../../Header';
 import VoteAmountSlider from './VoteAmountSlider';
-import { ArrowRightIcon } from '../../Icons';
+import {ArrowRightIcon} from '../../Icons';
+import {PopupModal} from "../../Content/PopupModal";
 
 const TronHttpClient = require('tron-http-client');
 
 class VoteDetails extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      rep: {},
-      wallets: [],
-      current: 0,
-      selectedWallet: {
-        text: 'Select a Wallet',
-        value: '',
-        frozenBalance: 0,
-      }
-    };
-  }
+        this.state = {
+            rep: {},
+            wallets: [],
+            current: 0,
+            selectedWallet: {
+                text: 'Select a Wallet',
+                value: '',
+                frozenBalance: 0,
+            },
 
-  componentDidMount() {
-    this.props.loadWitnesses();
-  }
+            sendProperties : {
 
-  async submitVote() {
-    let votes = [
-      {
-        address : this.state.rep.address,
-        count : parseInt(this.state.current)
-      }
-    ];
-
-    let client = new TronHttpClient();
-    let response = await client.vote(this.state.selectedWallet.privateKey, votes);
-    console.log(response);
-  }
-
-  selectWallet = (e, { value }) => {
-    let accounts = this.props.wallet.persistent.accounts;
-    let wallet = Object.keys(accounts).filter((wallet) => accounts[wallet].publicKey === value);
-    this.setState({ selectedWallet: accounts[wallet[0]] });
-  }
-
-  onSliderChange(amount) {
-    this.state.current = amount;
-    console.log(`amount: ${amount}`);
-  }
-
-  render() {
-    let { selectedWallet } = this.state;
-
-    let accountId = this.props.match.params.account;
-    let accounts = this.props.wallet.persistent.accounts;
-
-    let currentRep = atob(this.props.match.params.rep);
-    let rep = this.props.witnesses.witnesses.find(w => w.address === currentRep);
-    this.state.rep = rep;
-
-    if (!rep) {
-      return (
-        <div>not loaded</div>
-      );
+            }
+        };
     }
 
-    let wallets = [];
-    Object.keys(accounts).forEach((wallet, i) => {
-      let formattedObj = {
-        text: accounts[wallet].name,
-        value: accounts[wallet].publicKey
-      };
-      wallets.push(formattedObj);
-    });
+    componentDidMount() {
+        this.props.loadWitnesses();
+    }
 
-    return (
-      <Secondary className={styles.container}>
-        <div className={styles.headerContainer}>
-          <Header headerName="Votes" />
-          <div className={styles.headerTP}>{selectedWallet.frozenBalance}<span>TP</span></div>
-          <div className={styles.headerText}>Earn More TronPower by freezing Tron</div>
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.votingFor}>YOUR ARE VOTING FOR : <span>{rep.url}</span></div>
-          <div className={styles.dropdown}>
-            <ArrowRightIcon />
-            <Dropdown fluid selection
-                      onChange={this.selectWallet}
-                      placeholder="Choose Wallet"
-                      options={wallets}
-            />
-          </div>
-          <VoteAmountSlider onSliderChange={this.onSliderChange.bind(this)} totalTP={selectedWallet.frozenBalance} />
-          <Button onClick={this.submitVote.bind(this)} className={`${styles.btn} ${buttonStyles.button} ${buttonStyles.black}`}>Submit Your Vote</Button>
-        </div>
-      </Secondary>
-    );
-  }
+    async clickSubmit() {
+        let address = this.state.rep.address;
+        let count = parseInt(this.state.current);
+        this.setState({
+            ...this.state,
+            sendProperties : {
+                address : this.state.rep.address,
+                count : parseInt(this.state.current)
+            },
+            showConfirmModal : true,
+            modalConfirmText : `Submit ${count} Votes for ${this.state.rep.url} (${address} ?`
+        });
+    }
+
+    selectWallet = (e, {value}) => {
+        let accounts = this.props.wallet.persistent.accounts;
+        let wallet = Object.keys(accounts).filter((wallet) => accounts[wallet].publicKey === value);
+        this.setState({selectedWallet: accounts[wallet[0]]});
+    };
+
+    onSliderChange(amount) {
+        this.state.current = amount;
+        console.log(`amount: ${amount}`);
+    }
+
+    async modalConfirm() {
+        let votes = [
+            {
+                address: this.state.rep.address,
+                count: parseInt(this.state.current)
+            }
+        ];
+        let client = new TronHttpClient();
+        let response = await client.vote(this.state.selectedWallet.privateKey, votes).catch(x => null);
+
+        if (response === null) {
+            this.setState({
+                ...this.state,
+                sendProperties: {},
+                showConfirmModal: false,
+                showFailureModal: true,
+                modalFailureText: "Transaction failed"
+            });
+
+        } else if (response.result != true) {
+            this.setState({
+                ...this.state,
+                sendProperties: {},
+                showConfirmModal: false,
+                showFailureModal: true,
+                modalFailureText: "Transaction failed: " + response.message
+            });
+        } else {
+            this.setState({
+                ...this.state,
+                sendProperties: {},
+                showConfirmModal: false,
+                showSuccessModal: true,
+                modalSuccessText: "Transaction Successful!"
+            });
+        }
+
+        console.log(response);
+    }
+
+    modalDecline() {
+        this.setState({
+            ...this.state,
+            sendProperties: {},
+            showConfirmModal: false
+        });
+    }
+
+    modalFailureClose() {
+        this.setState({
+            ...this.state,
+            showFailureModal: false
+        });
+    }
+
+    modalSuccessClose() {
+        this.props.history.push("/wallets/walletDetails/" + this.state.accountAddress);
+        this.setState({
+            ...this.state,
+            showSuccessModal: false
+        });
+    }
+
+    modalClose() {
+        this.state.showConfirmModal = false;
+    }
+
+
+    render() {
+        let {selectedWallet} = this.state;
+
+        let accountId = this.props.match.params.account;
+        let accounts = this.props.wallet.persistent.accounts;
+
+        let currentRep = atob(this.props.match.params.rep);
+        let rep = this.props.witnesses.witnesses.find(w => w.address === currentRep);
+        this.state.rep = rep;
+
+        if (!rep) {
+            return (
+                <div>not loaded</div>
+            );
+        }
+
+        let wallets = [];
+        Object.keys(accounts).forEach((wallet, i) => {
+            let formattedObj = {
+                text: accounts[wallet].name,
+                value: accounts[wallet].publicKey
+            };
+            wallets.push(formattedObj);
+        });
+
+        return (
+            <Secondary className={styles.container}>
+                <div className={styles.headerContainer}>
+                    <Header headerName="Votes"/>
+                    <div className={styles.headerTP}>{selectedWallet.frozenBalance}<span>TP</span></div>
+                    <div className={styles.headerText}>Earn More TronPower by freezing Tron</div>
+                </div>
+                <div className={styles.subContainer}>
+                    <div className={styles.votingFor}>YOUR ARE VOTING FOR : <span>{rep.url}</span></div>
+                    <div className={styles.dropdown}>
+                        <ArrowRightIcon/>
+                        <Dropdown fluid selection
+                                  onChange={this.selectWallet}
+                                  placeholder="Choose Wallet"
+                                  options={wallets}
+                        />
+                    </div>
+                    <VoteAmountSlider onSliderChange={this.onSliderChange.bind(this)}
+                                      totalTP={selectedWallet.frozenBalance}/>
+                    <Button onClick={this.clickSubmit.bind(this)}
+                            className={`${styles.btn} ${buttonStyles.button} ${buttonStyles.black}`}>Submit Your
+                        Vote</Button>
+                </div>
+
+                <PopupModal
+                    confirmation
+                    modalVis={this.state.showConfirmModal}
+                    modalText={this.state.modalConfirmText}
+                    closeModalFunction={this.modalClose.bind(this)}
+                    modalConfirm={this.modalConfirm.bind(this)}
+                    modalDecline={this.modalDecline.bind(this)}
+                />
+
+                <PopupModal
+                    failure
+                    modalVis={this.state.showFailureModal}
+                    modalText={this.state.modalFailureText}
+                    closeModalFunction={this.modalFailureClose.bind(this)}
+                    modalConfirm={this.modalFailureClose.bind(this)}
+                />
+
+                <PopupModal
+                    success
+                    modalVis={this.state.showSuccessModal}
+                    modalText={this.state.modalSuccessText}
+                    closeModalFunction={this.modalSuccessClose.bind(this)}
+                    modalConfirm={this.modalSuccessClose.bind(this)}
+                />
+
+            </Secondary>
+        );
+    }
 }
 
 export default withRouter(connect(
-  state => ({ wallet: state.wallet, witnesses: state.witnesses }),
-  dispatch => ({
-    loadWitnesses: (props) => {
-      dispatch(loadWitnesses(props));
-    }
-  })
+    state => ({wallet: state.wallet, witnesses: state.witnesses}),
+    dispatch => ({
+        loadWitnesses: (props) => {
+            dispatch(loadWitnesses(props));
+        }
+    })
 )(VoteDetails));
--- app/components/Wallet/Send/SendAmount.js
+++ app/components/Wallet/Send/SendAmount.js
@@ -1,204 +1,201 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Button } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Button} from 'semantic-ui-react';
 import styles from './SendAmount.css';
 import Header from '../../Header';
 import AmountDisplay from './AmountDisplay';
 import buttonStyles from '../../Button.css';
-import { ContactIcon, BackArrowIcon } from '../../Icons';
+import {ContactIcon, BackArrowIcon} from '../../Icons';
 import {PopupModal} from '../../Content/PopupModal';
 
 import TronHttpClient from 'tron-http-client';
 
 class SendAmount extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-        amount: 0,
-        address: '',
+        this.state = {
+            amount: 0,
+            address: '',
 
-        showConfirmModal: false,
-        modalConfirmText : "",
+            showConfirmModal: false,
+            modalConfirmText: "",
 
-        target : "",
-        tokenStr : "",
+            target: "",
+            tokenStr: "",
 
-        sendProperties : {
+            sendProperties: {},
 
-        },
+            showFailureModal: false,
+            modalFailureText: "Transaction Failed",
 
-        showFailureModal : false,
-        modalFailureText : "Transaction Failed",
+            showSuccessModal: false,
+            modalSuccessText: "Success",
+            accountAddress: ""
+        };
+    }
 
-        showSuccessModal: false,
-        modalSuccessText : "Success",
-        accountAddress : ""
-    };
-  }
-
-  async onClickSend() {
-    let accountId = this.props.match.params.account;
-    let account = this.props.wallet.persistent.accounts[accountId];
+    async onClickSend() {
+        let accountId = this.props.match.params.account;
+        let account = this.props.wallet.persistent.accounts[accountId];
 
 
-    this.setState({
-        ...this.state,
-        sendProperties : {
-            privateKey : account.privateKey,
-            recipient : this.state.address.trim(),
-            amount : parseInt(this.state.amount)
-        },
-        accountAddress : account.publicKey,
-        showConfirmModal : true,
-        modalConfirmText: `Send ${this.state.amount} ${this.state.tokenStr} to ${this.state.address}?`
-    });
+        this.setState({
+            ...this.state,
+            sendProperties: {
+                privateKey: account.privateKey,
+                recipient: this.state.address.trim(),
+                amount: parseInt(this.state.amount)
+            },
+            accountAddress: account.publicKey,
+            showConfirmModal: true,
+            modalConfirmText: `Send ${this.state.amount} ${this.state.tokenStr} to ${this.state.address}?`
+        });
 
-  }
+    }
 
-  onSetAmount(amount) {
-      this.setState({
-          ...this.state,
-          amount : amount
-      })
-  }
+    onSetAmount(amount) {
+        this.setState({
+            ...this.state,
+            amount: amount
+        })
+    }
 
-  onSetAddress(event) {
-      this.setState({
-          ...this.state,
-          address : event.target.value
-      });
-  }
+    onSetAddress(event) {
+        this.setState({
+            ...this.state,
+            address: event.target.value
+        });
+    }
 
-  async modalConfirm(){
-      let client = new TronHttpClient();
-      let response = null;
-      if (this.props.match.params.token) {
-          response = await client.sendToken(
-              this.state.sendProperties.privateKey,
-              this.state.sendProperties.recipient,
-              this.state.sendProperties.amount,
-              this.props.match.params.token).catch(x => null);
-      } else {
-          console.log(this.state.sendProperties);
-          response = await client.sendTrx(
-              this.state.sendProperties.privateKey,
-              this.state.sendProperties.recipient,
-              this.state.sendProperties.amount).catch(x => null);
-      }
+    async modalConfirm() {
+        let client = new TronHttpClient();
+        let response = null;
+        if (this.props.match.params.token) {
+            response = await client.sendToken(
+                this.state.sendProperties.privateKey,
+                this.state.sendProperties.recipient,
+                this.state.sendProperties.amount,
+                this.props.match.params.token).catch(x => null);
+        } else {
+            console.log(this.state.sendProperties);
+            response = await client.sendTrx(
+                this.state.sendProperties.privateKey,
+                this.state.sendProperties.recipient,
+                this.state.sendProperties.amount).catch(x => null);
+        }
 
-      if(response === null){
-          this.setState({
-              ...this.state,
-              sendProperties:{},
-              showConfirmModal:false,
-              showFailureModal:true,
-              modalFailureText : "Transaction failed"
-          });
+        if (response === null) {
+            this.setState({
+                ...this.state,
+                sendProperties: {},
+                showConfirmModal: false,
+                showFailureModal: true,
+                modalFailureText: "Transaction failed"
+            });
 
-      }else if (response.result != true){
-          this.setState({
-              ...this.state,
-              sendProperties:{},
-              showConfirmModal:false,
-              showFailureModal:true,
-              modalFailureText : "Transaction failed: " + response.message
-          });
-      }else{
-          this.setState({
-              ...this.state,
-              sendProperties:{},
-              showConfirmModal:false,
-              showSuccessModal:true,
-              modalSuccessText: "Transaction Successful!"
-          });
-      }
+        } else if (response.result != true) {
+            this.setState({
+                ...this.state,
+                sendProperties: {},
+                showConfirmModal: false,
+                showFailureModal: true,
+                modalFailureText: "Transaction failed: " + response.message
+            });
+        } else {
+            this.setState({
+                ...this.state,
+                sendProperties: {},
+                showConfirmModal: false,
+                showSuccessModal: true,
+                modalSuccessText: "Transaction Successful!"
+            });
+        }
 
-      console.log(response);
-  }
+        console.log(response);
+    }
 
-  modalDecline(){
-      this.setState({
-          ...this.state,
-          sendProperties:{},
-          showConfirmModal:false
-      });
-  }
+    modalDecline() {
+        this.setState({
+            ...this.state,
+            sendProperties: {},
+            showConfirmModal: false
+        });
+    }
 
-  modalFailureClose(){
-      this.setState({
-          ...this.state,
-          showFailureModal:false
-      });
-  }
+    modalFailureClose() {
+        this.setState({
+            ...this.state,
+            showFailureModal: false
+        });
+    }
 
-  modalSuccessClose(){
-      this.props.history.push("/wallets/walletDetails/" + this.state.accountAddress);
-      this.setState({
-          ...this.state,
-          showSuccessModal:false
-      });
-  }
+    modalSuccessClose() {
+        this.props.history.push("/wallets/walletDetails/" + this.state.accountAddress);
+        this.setState({
+            ...this.state,
+            showSuccessModal: false
+        });
+    }
 
-  modalClose(){
-      this.state.showConfirmModal = false;
-  }
+    modalClose() {
+        this.state.showConfirmModal = false;
+    }
 
-  render() {
-    let token = (this.props.match.params.token ? this.props.match.params.token : 'TRX');
-    this.state.tokenStr = token;
-    return (
-      <div className={styles.container}>
-        <Header className={styles.white} headerName="Enter Amount" />
-        <div onClick={this.props.history.goBack} className={styles.backArrow}>
-          <BackArrowIcon />
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.addressContainer}>
-            <ContactIcon />
-            <input
-              onChange={this.onSetAddress.bind(this)}
-              placeholder="Recipient Address"
-              className={styles.address}
-              value={this.props.address}
-            />
-          </div>
-          <AmountDisplay token={token} onSetAmount={this.onSetAmount.bind(this)} />
-          <Button
-            onClick={this.onClickSend.bind(this)}
-            className={`${buttonStyles.button} ${buttonStyles.black}`}>Send
-          </Button>
+    render() {
+        let token = (this.props.match.params.token ? this.props.match.params.token : 'TRX');
+        this.state.tokenStr = token;
+        return (
+            <div className={styles.container}>
+                <Header className={styles.white} headerName="Enter Amount"/>
+                <div onClick={this.props.history.goBack} className={styles.backArrow}>
+                    <BackArrowIcon/>
+                </div>
+                <div className={styles.subContainer}>
+                    <div className={styles.addressContainer}>
+                        <ContactIcon/>
+                        <input
+                            onChange={this.onSetAddress.bind(this)}
+                            placeholder="Recipient Address"
+                            className={styles.address}
+                            value={this.props.address}
+                        />
+                    </div>
+                    <AmountDisplay token={token} onSetAmount={this.onSetAmount.bind(this)}/>
+                    <Button
+                        onClick={this.onClickSend.bind(this)}
+                        className={`${buttonStyles.button} ${buttonStyles.black}`}>Send
+                    </Button>
 
+                    <PopupModal
+                        confirmation
+                        modalVis={this.state.showConfirmModal}
+                        modalText={this.state.modalConfirmText}
+                        closeModalFunction={this.modalClose.bind(this)}
+                        modalConfirm={this.modalConfirm.bind(this)}
+                        modalDecline={this.modalDecline.bind(this)}
+                    />
 
-          <PopupModal
-            confirmation
-            modalVis={this.state.showConfirmModal}
-            modalText={this.state.modalConfirmText}
-            closeModalFunction={this.modalClose.bind(this)}
-            modalConfirm={this.modalConfirm.bind(this)}
-            modalDecline={this.modalDecline.bind(this)}
-            />
+                    <PopupModal
+                        failure
+                        modalVis={this.state.showFailureModal}
+                        modalText={this.state.modalFailureText}
+                        closeModalFunction={this.modalFailureClose.bind(this)}
+                        modalConfirm={this.modalFailureClose.bind(this)}
+                    />
 
-            <PopupModal
-                failure
-                modalVis={this.state.showFailureModal}
-                modalText={this.state.modalFailureText}
-                closeModalFunction={this.modalFailureClose.bind(this)}
-                modalConfirm={this.modalFailureClose.bind(this)}
-            />
-
-            <PopupModal
-                success
-                modalVis={this.state.showSuccessModal}
-                modalText={this.state.modalSuccessText}
-                closeModalFunction={this.modalSuccessClose.bind(this)}
-                modalConfirm={this.modalSuccessClose.bind(this)}
-            />
-        </div>
-      </div>
-    );
-  }
+                    <PopupModal
+                        success
+                        modalVis={this.state.showSuccessModal}
+                        modalText={this.state.modalSuccessText}
+                        closeModalFunction={this.modalSuccessClose.bind(this)}
+                        modalConfirm={this.modalSuccessClose.bind(this)}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
-export default withRouter(connect(state => ({ wallet: state.wallet }))(SendAmount));
+export default withRouter(connect(state => ({wallet: state.wallet}))(SendAmount));
