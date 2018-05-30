b21ea22803a93f2dccd199aad92095efb42ca043
Tommy P <thomaspud@gmail.com>
2018-05-30 00:21:23 -0400

    Fixed: Vote Slider with TP, TP formatting on walletView

--- app/components/Vote/VoteDetails/VoteAmountSlider.js
+++ app/components/Vote/VoteDetails/VoteAmountSlider.js
@@ -18,17 +18,15 @@ export default class VoteDetails extends Component {
 
   get sliderWidthCalc() {
     if (this.state.current == 0 || this.props.totalTP == 0) {
-      // console.log('test')
       return 0
     }
-    // console.log('redraw')
     return Math.round((this.state.current / this.props.totalTP) * 100);
   }
 
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.amount}>{ /*parseInt(this.state.current).toLocaleString() */0} TP</div>
+        <div className={styles.amount}>{parseInt(this.state.current).toLocaleString()} TP</div>
         <div className={styles.sliderContainer}>
           <Input
             className={ styles.slider }
@@ -38,12 +36,12 @@ export default class VoteDetails extends Component {
             type="range"
             value={this.state.current}
           />
-          <div className={styles.progress} style={{width: /*this.sliderWidthCalc*/0 + '%'}}></div>
+          <div className={styles.progress} style={{width: this.sliderWidthCalc + '%'}}></div>
           <div className={styles.sliderBG}></div>
         </div>
         <div className={styles.sliderRange}>
           <span>0 TP</span>
-          <span>{ this.props.totalTP/*.toLocaleString()*/ } TP</span>
+          <span>{ this.props.totalTP.toLocaleString() } TP</span>
         </div>
       </div>
     );
--- app/components/Wallet/WalletView/SubHeader.js
+++ app/components/Wallet/WalletView/SubHeader.js
@@ -8,7 +8,7 @@ export default class SubHeader extends Component {
     return (
       <div className={styles.main}>
         <div className={styles.mainAmount}>{ (this.props.account.trx / 100000000).toFixed(8) } <span>TRX</span></div>
-        <div className={styles.tronPower}>{ (this.props.account.frozenBalance / 100000000).toFixed(8) } <span>TP</span></div>
+        <div className={styles.tronPower}>{ this.props.account.frozenBalance } <span>TP</span></div>
         <div className={styles.tpExpire}>Expires: <span> { new Date(this.props.account.frozenExpireTime).toLocaleString() }</span></div>
         <div className={styles.scroll}>
           <div className={styles.container}>
