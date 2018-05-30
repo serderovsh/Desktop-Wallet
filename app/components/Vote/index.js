5660e30de4a99c4d68fb2b85072099ba46a17277
Tommy P <thomaspud@gmail.com>
2018-05-30 12:40:18 -0400

    Added: VoteList Filter Transition

--- app/components/Vote/index.js
+++ app/components/Vote/index.js
@@ -8,6 +8,8 @@ import { MoreIcon, WalletIcon, DownloadIcon } from '../Icons';
 import Header from '../ContentPrimaryHeader';
 import Vote from './Vote';
 
+import { CSSTransitionGroup } from 'react-transition-group';
+
 import styles from './VoteList.css';
 
 class VoteList extends Component {
@@ -45,19 +47,25 @@ class VoteList extends Component {
         <input className={styles.input} placeholder="Search for a Witness here..." onChange={this.filterTokens} />
         <div className={styles.votesContainer}>
           {witnesses.length < 1 ? (<div className={styles.noResults}>No Witnesses Found</div>) : ''}
-          {
-            witnesses.map((rep, i) =>
-              <Vote
-                key={i}
-                voteLabel={i + 1}
-                voteTitle={rep.url}
-                lastBlock={rep.latestblocknum}
-                blocksProduced={rep.totalproduced}
-                blocksMissed={rep.totalmissed}
-                totalVote={rep.votecount}
-                address={rep.address}
-              />)
-          }
+          <CSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}
+          >
+            {
+              witnesses.map((rep, i) =>
+                <Vote
+                  key={i}
+                  voteLabel={i + 1}
+                  voteTitle={rep.url}
+                  lastBlock={rep.latestblocknum}
+                  blocksProduced={rep.totalproduced}
+                  blocksMissed={rep.totalmissed}
+                  totalVote={rep.votecount}
+                  address={rep.address}
+                />)
+            }
+          </CSSTransitionGroup >
         </div>
       </div>
     );
