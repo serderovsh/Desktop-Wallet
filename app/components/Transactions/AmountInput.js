d1704d78debb970c92489ce337d83adb485aa90e
Tommy P <thomaspud@gmail.com>
2018-05-31 05:30:08 -0400

    Added: Transactions Transfer

---
+++ app/components/Transactions/AmountInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class AmountInput extends Component {
+  onChange(event) {
+    this.props.onSetAmount(event.target.value);
+  }
+
+  render() {
+    return (
+      <input onChange={this.onChange.bind(this)} type="number" placeholder="0.00000000" required />
+    );
+  }
+}
