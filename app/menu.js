c44944ac0923bc733fab9c4c7cdb45b34a4e5587
Antoine Portaria <antoine@hakouna.com>
2018-06-21 12:16:28 -0400

    Added closing button for MacOS and Fixed the starting width of the application

--- app/menu.js
+++ app/menu.js
@@ -47,16 +47,24 @@ export default class MenuBuilder {
     const subMenuEdit = {
       label: "Edit",
       submenu: [
-        { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "Command+X", selector: "cut:" },
-        { label: "Copy", accelerator: "Command+C", selector: "copy:" },
-        { label: "Paste", accelerator: "Command+V", selector: "paste:" },
         {
-          label: "Select All",
-          accelerator: "Command+A",
-          selector: "selectAll:"
+          label: 'Hide ElectronReact',
+          accelerator: 'Command+H',
+          selector: 'hide:'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          selector: 'hideOtherApplications:'
+        },
+        { label: 'Show All', selector: 'unhideAllApplications:' },
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => {
+            app.quit();
+          }
         }
       ]
     };
