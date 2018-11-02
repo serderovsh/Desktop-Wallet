5a3d48238d3d2dc9d3e9c1ec349e3647912381c0
Shamsudin <steein.shamsudin@gmail.com>
2018-11-02 21:04:17 +0300

    Create ru.js

---
+++ app/translations/ru.js
@@ -0,0 +1,10 @@
+export const messages = {
+  app_title: "TronWatch",
+  name: "Название",
+  wallets: "Кошельки",
+  tokens: "Токены",
+  vote: "Голоса",
+  contacts: "Смарт-Контакты",
+  settings: "Настройки",
+  create_new_wallet: "Создать кошелек"
+};
